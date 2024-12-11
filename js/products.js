7e115ed50ef555052759233d64cc7dee7e372141
Sahitya hp <neupanesahitya1@gmail.com>
2024-12-10 19:27:14 -0500

    added all the product data and product template

---
+++ js/products.js
@@ -0,0 +1,27 @@
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("id");
+console.log(params, productId);
+let product;
+const loadProduct = async () => {
+  try {
+    const response = await fetch("../data/product/product.json");
+    const products = await response.json();
+    product = products[productId];
+  } catch (err) {
+    console.log(err);
+  }
+  if (product) {
+    document.getElementById("product-name").textContent = product.name;
+    document.getElementById("product-image").src = product.image;
+    document.getElementById("product-description").textContent =
+      product.description;
+    document.getElementById("product-price").textContent = product.price;
+  } else {
+    document.querySelector(
+      ".container"
+    ).innerHTML = `<p class="product-not-found">❌ Product not found.</p>`;
+  }
+};
+
+loadProduct();
--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 const header = document.querySelector("header"),
-  footer = document.querySelector("footer");
-
-console.log(header, footer);
+  footer = document.querySelector("footer"),
+  tickerSection = document.querySelector(".ticker-section");
 
 window.onload = async () => {
+  const path = window.location.pathname;
   const headerComponent = await loadHtml("../view/shared/header.html");
   const footerComponent = await loadHtml("../view/shared/footer.html");
 
   header.innerHTML = headerComponent;
   footer.innerHTML = footerComponent;
+  console.log(path);
+  // ticker will only in homepage
+  if (path === "/" || path === "/index.html") {
+    const tickerComponent = await loadHtml("./view/ticker.html");
+    tickerSection.innerHTML += tickerComponent;
+  } else {
+    return null;
+  }
 };
 // function created to to load html
 const loadHtml = (location) => {
-  console.log(location);
   const html = fetch(location)
     .then((data) => {
       console.log(data);
